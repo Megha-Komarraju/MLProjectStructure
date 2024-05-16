182a15ffa8482fc30237c7761ed87bd7b7bb45d2
Megha Komarraju <meghakomarraju@gmail.com>
2024-05-16 14:12:41 -0400

    The Second commit- setup and requirements.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+## Make your Machine Learning model as a package so you can install it wherever required and use it.
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements= [req.replace("\n","") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+        
+        
+    
+    
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Megha',
+author_email='meghakomarraju@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
